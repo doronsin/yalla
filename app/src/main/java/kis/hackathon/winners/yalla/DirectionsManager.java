f92c0cc82a57b4d786747e74dc01b071a2198b2f
Doron Singer <doronsin@gmail.com>
2016-06-09 17:40:40 +0300

    added directions manager

--- app/src/main/java/kis/hackathon/winners/yalla/DirectionsManager.java
+++ app/src/main/java/kis/hackathon/winners/yalla/DirectionsManager.java
@@ -1,14 +1,18 @@
 package kis.hackathon.winners.yalla;
-import android.graphics.Point;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import java.io.IOException;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import com.google.gson.JsonObject;
+
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.LatLng;
 
 /**
  *
@@ -18,38 +22,32 @@ public class DirectionsManager {
     private static final String TAG = DirectionsManager.class.getSimpleName();
     public static final String SERVER_URL = "https://maps.googleapis.com/maps/api/directions/json?origin=";
     private static final String GOOGLE_KEY = "AIzaSyBJK_IDdOZOegnfCPCBn4d016TpVWrBz44";
-    public void sendRequest(final Place origin, final Place dest, final TimeReady caller) {
-        new AsyncTask<Void, Void, Integer>() {
-            OkHttpClient client = new OkHttpClient();
-            @Override
-            protected Integer doInBackground(Void... params) {
-                try {
-                    RequestBody formBody = new FormBody.Builder().build();
-                    String requestUrl = getFullUrl(origin, dest);
-                    Request request = new Request.Builder().url(requestUrl).post(formBody).build();
-                    Response response = client.newCall(request).execute();
+    public static final int PERMISSION_INTERNET = 3;
 
-                    if (!response.isSuccessful()) {
-                        throw new IOException("Unexpected Code: " + response);
-                    } else {
-                        String retVal = response.body().string();
-                        Log.d(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.d(TAG, e.toString(), e.getCause());
-                }
-                return 1;
+
+    public static void sendRequest(final LatLng origin, final LatLng dest, final TimeReady caller) throws Exception {
+        DirectionsApiRequest req = getDirections(origin, dest, caller);
+        req.await();
+    }
+    private static DirectionsApiRequest getDirections(LatLng origin, LatLng destination, final TimeReady caller) {
+        Log.d(TAG, "lkasdjflkaszjdlkfjaslkdfjlasdf");
+        GeoApiContext key = new GeoApiContext();
+        key.setApiKey(GOOGLE_KEY);
+        DirectionsApiRequest req = DirectionsApi.newRequest(key);
+        req = req.origin(origin).destination(destination);
+        req.setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                caller.onTimeReady((int)result.routes[0].legs[0].duration.inSeconds/60);
             }
 
             @Override
-            protected void onPostExecute(Integer integer) {
-                caller.onTimeReady(integer.intValue());
+            public void onFailure(Throwable e) {
+                Log.d(TAG, "failed receiving routess!"+e.getMessage());
             }
-        }.execute();
+        });
+        return req;
     }
 
-    private String getFullUrl(Place origin, Place dest) {
-        return SERVER_URL + origin.getX() + ", " + origin.getY() + "&destination=" + dest.getX() + ", " + dest.getY() + "&key=" + GOOGLE_KEY;
-    }
 }
 
--- app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
+++ app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
@@ -1,27 +1,35 @@
 package kis.hackathon.winners.yalla;
 
 import android.graphics.Point;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.maps.DirectionsApi;
+import com.google.maps.model.LatLng;
+
 public class MainActivity extends AppCompatActivity implements TimeReady {
 
-    private static final String TAG = "main_activity6";
+    private static final String TAG = "main_activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DirectionsManager x = new DirectionsManager();
+        Log.d(TAG, "before!");
+        try {
+            DirectionsManager.sendRequest(new LatLng(31.750876, 35.223831), new LatLng(29.580795, 34.946855), this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //https://maps.googleapis.com/maps/api/directions/json?origin=31.750876,%2035.223831&destination=31.774577,%2035.197986&key=AIzaSyBJK_IDdOZOegnfCPCBn4d016TpVWrBz44
-        x.sendRequest(new Place(31.750876, 35.223831), new Place(31.774577, 35.197986), this);
     }
 
     @Override
     public void onTimeReady(int timeUntilArrive) {
         Log.d(TAG, "time ready!!!" + timeUntilArrive);
     }
-
 
 }
