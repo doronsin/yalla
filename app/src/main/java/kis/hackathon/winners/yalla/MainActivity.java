6a1b070505272d63666f832cbb141897369132a0
Odelya Krief <odelya1232@gmail.com>
2016-06-10 05:23:31 +0300

    kkk

--- app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
+++ app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "main_activity";
     private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 789;
+    private static final String SMS_MINUTES_TEXT ="SMS will be sent %d minutes\n before you arrive" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 seekBarProgress = progress;
+                updateMinutesTranceText(progress);
 
             }
 
@@ -45,8 +49,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void updateMinutesTranceText(int progress) {
+        ImageView man = (ImageView) findViewById(R.id.iv_man);
+        ImageView woman = (ImageView) findViewById(R.id.iv_woman);
+        TextView textView= (TextView) findViewById(R.id.tv_minutes);
+        float womanAlph = Math.min(1,(float)(progress+50)/100);
+        float manAlph = Math.min(1, (1 - progress + 50) / 100);
+        Log.d(TAG,"manAlph:"+manAlph+ " womanAlph"+womanAlph);
+        man.setAlpha(manAlph);
+        woman.setAlpha(womanAlph);
+        if (between(0,20,progress)){
+            textView.setText("SMS will be sent 2 minutes\n before you arrive");
+            return;
+        }
+        if (between(20,40,progress)){
+            textView.setText(String.format(SMS_MINUTES_TEXT,2));
 
-        @Override
+
+            return;
+        }
+        if (between(40,60,progress)){
+            textView.setText(String.format(SMS_MINUTES_TEXT,4));
+            return;
+        }
+        if (between(60,80,progress)){
+
+            textView.setText(String.format(SMS_MINUTES_TEXT,6));
+            return;
+        }
+        if (between(80,100,progress)){
+
+            textView.setText(String.format(SMS_MINUTES_TEXT,8));
+            return;
+        }
+    }
+    private boolean between(int start, int end, int numb){
+        return start<= numb && numb<end;
+    }
+
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == SmsSender.REQ_PERMISSION_SEND_SMS) {
             //TODO IF PERMISSION DENIED...
