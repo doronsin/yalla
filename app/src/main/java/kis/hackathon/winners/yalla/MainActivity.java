94e070124fd23eabf44e0cba76a9b2f6a2350fe8
Odelya Krief <odelya1232@gmail.com>
2016-06-10 03:38:50 +0300

    stable seekbar

--- app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
+++ app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
@@ -1,9 +1,12 @@
 package kis.hackathon.winners.yalla;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,15 +23,30 @@ public class MainActivity extends AppCompatActivity {
 //          PermissionAsker.askForPermission(this,Manifest.permission.SEND_SMS,SmsSender.REQ_PERMISSION_SEND_SMS);
 //      SmsSender.getInstance().sendSms(2, "0535246156");
         AutoPlaces.populateAutoPlaces(this);
+       SeekBar seekBar=((SeekBar) findViewById(R.id.sb_ma));
+        //seekBar.getThumb().setColorFilter(getColor(R.color.gray), PorterDuff.Mode.MULTIPLY);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int seekBarProgress = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarProgress = progress;
+
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
 
 
-
-
-
+        });
     }
 
 
-    @Override
+        @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == SmsSender.REQ_PERMISSION_SEND_SMS) {
             //TODO IF PERMISSION DENIED...
---
+++ app/src/main/java/kis/hackathon/winners/yalla/TableItem.java
@@ -0,0 +1,16 @@
+package kis.hackathon.winners.yalla;
+
+/**
+ * Created by odelya_krief on 09-Jun-16.
+ */
+public class TableItem {
+    public String phone;
+    public String address;
+    public int time;
+
+    public TableItem(String p_phone, String p_address, int p_time) {
+        phone=p_phone;
+        address=p_address;
+        time=p_time;
+    }
+}
