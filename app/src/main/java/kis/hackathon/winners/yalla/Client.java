628ee3ea060251ad4924980e27d0708cedbc8a3e
Doron Singer <doronsin@gmail.com>
2016-06-10 09:34:28 +0300

    db

--- app/src/main/java/kis/hackathon/winners/yalla/Client.java
+++ app/src/main/java/kis/hackathon/winners/yalla/Client.java
@@ -44,35 +44,32 @@ public class Client {
         }.execute();
     }
 
-    private void getAddress(final String number, final ) {
-        ​
-        new AsyncTask<Void, Void, Void>() {
-            OkHttpClient client = new OkHttpClient();
-            ​
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    RequestBody formBody = new FormBody.Builder()
-                            .add("number", number)
-                            .build();
-                    ​
-                    Request request = new Request.Builder()
-                            .url(SERVER_URL)
-                            .post(formBody)
-                            .build();
-                    ​
-                    Response response = client.newCall(request).execute();
-                    if (!response.isSuccessful()) {
-                        throw new IOException("Unexpected Code: " + response);
-                    } else {
-                        Log.d(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.d(TAG, e.toString(), e.getCause());
-                }
-                return null;
-            }
-        }.execute();
+    private void getAddress(final String number) {
+//        new AsyncTask<Void, Void, Void>() {
+//            OkHttpClient client = new OkHttpClient();
+//            ​
+//            @Override
+//            protected Void doInBackground(Void... params) {
+//                try {
+//                    RequestBody formBody = new FormBody.Builder()
+//                            .add("number", number)
+//                            .build();
+//                    Request request = new Request.Builder()
+//                            .url(SERVER_URL)
+//                            .post(formBody)
+//                            .build();
+//                    ​
+//                    Response response = client.newCall(request).execute();
+//                    if (!response.isSuccessful()) {
+//                        throw new IOException("Unexpected Code: " + response);
+//                    } else {
+//                        Log.d(TAG, response.body().string());
+//                    }
+//                } catch (IOException e) {
+//                    Log.d(TAG, e.toString(), e.getCause());
+//                }
+//                return null;
+//            }
+//        }.execute();
     }
-}
 }
--- app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
+++ app/src/main/java/kis/hackathon/winners/yalla/MainActivity.java
@@ -1,24 +1,15 @@
 package kis.hackathon.winners.yalla;
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import android.Manifest;
-import android.graphics.Point;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -27,146 +18,18 @@ import com.google.maps.model.LatLng;
 import android.view.View;
 import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity implements TimeReady implements AddressReady {
-
-    private static final String TAG = "main_activity";
-    private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 789;
-    private TextView resultText;
-    private SearchView searchView;
-    private String _nameOfContact;
-    private String _phoneNumber;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "main222";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_auto_place);
-        Log.d(TAG, "starting");
-//        if(!SmsSender.getInstance().isAuthorised())
-//          PermissionAsker.askForPermission//(this,Manifest.permission.SEND_SMS,SmsSender.REQ_PERMISSION_SEND_SMS);
-//   	SmsSender.getInstance().sendSms(2, "0535246156");
-        startService(new Intent(this, GPSservice.class));
-        Log.d(TAG, "starting");
-//        if(!SmsSender.getInstance().isAuthorised())
-//          PermissionAsker.askForPermission(this,Manifest.permission.SEND_SMS,SmsSender.REQ_PERMISSION_SEND_SMS);
-//      SmsSender.getInstance().sendSms(2, "0535246156");
-//        AutoPlaces.populateAutoPlaces(this);
-
-        Log.d(TAG, "before!");
-        try {
-            DirectionsManager.sendRequest(new LatLng(31.750876, 35.223831), new LatLng(29.580795, 34.946855), this);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        resultText = (TextView)findViewById(R.id.searchViewResult);
-        setupSearchView();
-        AutoPlaces.populateAutoPlaces(this);
-       SeekBar seekBar=((SeekBar) findViewById(R.id.sb_ma));
-        //seekBar.getThumb().setColorFilter(getColor(R.color.gray), PorterDuff.Mode.MULTIPLY);
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int seekBarProgress = 0;
-
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                seekBarProgress = progress;
-
-        //https://maps.googleapis.com/maps/api/directions/json?origin=31.750876,%2035.223831&destination=31.774577,%2035.197986&key=AIzaSyBJK_IDdOZOegnfCPCBn4d016TpVWrBz44
-    }
-    private void setupSearchView() {
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) findViewById(R.id.searchView);
-        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
-        searchView.setSearchableInfo(searchableInfo);
-    }
-=======
-            }
-
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, //String[] permissions, int[] grantResults) {
-//        if (requestCode == SmsSender.REQ_PERMISSION_SEND_SMS) //{
-//            //TODO IF PERMISSION DENIED...
-//
-//        }
-//        else
-//            super.onRequestPermissionsResult(requestCode, //permissions, grantResults);
-//
-//
-//        Log.d(TAG, "before!");
-//        try {
-//            DirectionsManager.sendRequest(new LatLng//(31.750876, 35.223831), new LatLng(29.580795, 34.946855), //this);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        //https://maps.googleapis.com/maps/api/directions/json?//origin=31.750876,%2035.223831&destination=31.774577,//%2035.197986&key=AIzaSyBJK_IDdOZOegnfCPCBn4d016TpVWrBz44
-//
-//    }
-//
-    @Override
-    public void onTimeReady(int timeUntilArrive) {
-        Log.d(TAG, "time ready!!!" + timeUntilArrive);
-=======
-            }
-
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-
-
-        });
-    }
-    @Override
-    protected void onNewIntent(Intent intent) {
-        if (ContactsContract.Intents.SEARCH_SUGGESTION_CLICKED.equals(intent.getAction())) {
-            //handles suggestion clicked query
-            String displayName = getDisplayNameForContact(intent);
-            resultText.setText(displayName);
-        } else if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            // handles a search query
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            resultText.setText("should search for query: '" + query + "'...");
-        }
-
-    }
-    private String getDisplayNameForContact(Intent intent) {
-        Cursor phoneCursor = getContentResolver().query(intent.getData(), null, null, null, null);
-        phoneCursor.moveToFirst();
-        int idDisplayName = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
-        int hasNumber = phoneCursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
-        String s = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-//        int idDisplayName = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
-        _nameOfContact = phoneCursor.getString(idDisplayName);
-        _phoneNumber = getPhoneNumber(_nameOfContact);
-        // TODO call SmsSender and use the phone number
-
-        @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == SmsSender.REQ_PERMISSION_SEND_SMS) {
-            //TODO IF PERMISSION DENIED...
-
-        phoneCursor.close();
-        return _nameOfContact;
-    }
-    private String getPhoneNumber(String name) {
-        String ret = null;
-        String selection = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " like'" + name + "'";
-        String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
-        Cursor c = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                projection, selection, null, null);
-        if (c != null) {
-            if (c.moveToFirst()) {
-                ret = c.getString(0);
-            }
-            c.close();
-        }
-        if (ret == null) {
-            ret = "Unsaved";
-        }
-        return ret;
-    }
-
-    @Override
-    public void onTimeFailure() {
-        Log.d(TAG, "time failure");
+        Log.d(TAG, "test");
+        MyDataBase x = new MyDataBase(this, "address", null, 1);
+        Log.d(TAG, "test2");
+        TableItem y = x.getItem("0505708855");
+        Log.d(TAG, y.address);
+//        x.insert(new TableItem("0505708855", "beytar", 5));
     }
 }
