6814afe055887b3977c8af8ccd55eb279609893d
reemhalamish <reem.halamish@gmail.com>
2017-02-21 16:17:51 +0200

    fixed bug intentService is getting killed right after calling something async and staying without work

--- app/src/main/java/kis/hackathon/winners/yalla/GPSservice.java
+++ app/src/main/java/kis/hackathon/winners/yalla/GPSservice.java
@@ -1,11 +1,14 @@
 package kis.hackathon.winners.yalla;
 
 import android.Manifest;
-import android.app.IntentService;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -23,7 +26,7 @@ import com.google.maps.model.LatLng;
  * Created by odelya_krief on 09-Jun-16.
  * main thread. will send the SMSs
  */
-public class GPSservice extends IntentService implements
+public class GPSservice extends Service implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener,
@@ -36,11 +39,6 @@ public class GPSservice extends IntentService implements
 
     private GoogleApiClient mGoogleApiClient;
 
-
-    public GPSservice() {
-        super("GPService");
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -52,10 +50,69 @@ public class GPSservice extends IntentService implements
         mGoogleApiClient.connect();
     }
 
-
+    /**
+     * Called by the system every time a client explicitly starts the service by calling
+     * {@link Context#startService}, providing the arguments it supplied and a
+     * unique integer token representing the start request.  Do not call this method directly.
+     * <p>
+     * <p>For backwards compatibility, the default implementation calls
+     * {@link #onStart} and returns either {@link #START_STICKY}
+     * or {@link #START_STICKY_COMPATIBILITY}.
+     * <p>
+     * <p>If you need your application to run on platform versions prior to API
+     * level 5, you can use the following model to handle the older {@link #onStart}
+     * callback in that case.  The <code>handleCommand</code> method is implemented by
+     * you as appropriate:
+     * <p class="caution">Note that the system calls this on your
+     * service's main thread.  A service's main thread is the same
+     * thread where UI operations take place for Activities running in the
+     * same process.  You should always avoid stalling the main
+     * thread's event loop.  When doing long-running operations,
+     * network calls, or heavy disk I/O, you should kick off a new
+     * thread, or use {@link AsyncTask}.</p>
+     *
+     * @param intent  The Intent supplied to {@link Context#startService},
+     *                as given.  This may be null if the service is being restarted after
+     *                its process has gone away, and it had previously returned anything
+     *                except {@link #START_STICKY_COMPATIBILITY}.
+     * @param flags   Additional data about this start request.  Currently either
+     *                0, {@link #START_FLAG_REDELIVERY}, or {@link #START_FLAG_RETRY}.
+     * @param startId A unique integer representing this specific request to
+     *                start.  Use with {@link #stopSelfResult(int)}.
+     * @return The return value indicates what semantics the system should
+     * use for the service's current started state.  It may be one of the
+     * constants associated with the {@link #START_CONTINUATION_MASK} bits.
+     * @see #stopSelfResult(int)
+     */
     @Override
-    protected void onHandleIntent(Intent intent) {
-        Log.d(TAG, "started GPS service");
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    /**
+     * Return the communication channel to the service.  May return null if
+     * clients can not bind to the service.  The returned
+     * {@link IBinder} is usually for a complex interface
+     * that has been <a href="{@docRoot}guide/components/aidl.html">described using
+     * aidl</a>.
+     * <p>
+     * <p><em>Note that unlike other application components, calls on to the
+     * IBinder interface returned here may not happen on the main thread
+     * of the process</em>.  More information about the main thread can be found in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
+     * Threads</a>.</p>
+     *
+     * @param intent The Intent that was used to bind to this service,
+     *               as given to {@link Context#bindService
+     *               Context.bindService}.  Note that any extras that were included with
+     *               the Intent at that point will <em>not</em> be seen here.
+     * @return Return an IBinder through which clients can call on to the
+     * service.
+     */
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
@@ -103,7 +160,7 @@ public class GPSservice extends IntentService implements
         disconnectLocations();
         DirectionsManager.sendRequest(
                 new LatLng(location.getLatitude(), location.getLongitude()),
-                YallaSmsManager.getInstance().get_dest(),
+                YallaSmsManager.getInstance().getDest(),
                 this
         ); // will be sent and then passed to "onTimeReady"
 
@@ -127,16 +184,16 @@ public class GPSservice extends IntentService implements
 
     @Override
     public void onTimeReady(int timeUntilArrive) {
-        int minutesToSleep = timeUntilArrive - YallaSmsManager.getInstance().get_minutesToArrive();
+        int minutesToSleep = timeUntilArrive - YallaSmsManager.getInstance().getMinutesToArrive();
         Log.d(TAG, "onTimeReady, minutes until arrive: "+timeUntilArrive);
         if (minutesToSleep <= 0) {
-            YallaSmsManager.getInstance().set_minutesToArrive(timeUntilArrive);
+            YallaSmsManager.getInstance().setMinutesToArrive(timeUntilArrive);
             YallaSmsManager.getInstance().sendSms();
             Log.d(TAG, "sms sent!");
             killMyself();
         } else {
             try {
-                Log.d(TAG, "sleeping for "+minutesToSleep+" minutes. minutes now is "+ SystemClock.currentThreadTimeMillis());
+                Log.d(TAG, "sleeping for "+minutesToSleep+" minutes. milisec from initiation are "+ SystemClock.uptimeMillis());
                 Thread.sleep(minutesToSleep * 60 * 1000);
                 Log.d(TAG, "minutes now is "+ SystemClock.currentThreadTimeMillis());
                 mGoogleApiClient.connect();
